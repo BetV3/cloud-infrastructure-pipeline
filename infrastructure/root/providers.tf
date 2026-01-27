75d7aa34dd47f736e21e429a5448f449730b47cc
BetV3 <elvisramirez999@gmail.com>
2026-01-27 00:22:57 -0600

    Add initial Terraform configuration for VPC, outputs, providers, variables, and versions

---
+++ infrastructure/root/providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = var.aws_region
+
+    default_tags {
+      tags = merge(
+        var.tags,
+        {
+            Environment = var.environment
+            ManagedBy = "terraform"
+            Project = "cloud-infrastructure-pipeline"
+        }
+      )
+    }
+}
